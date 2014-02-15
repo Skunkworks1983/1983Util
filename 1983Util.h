a4469106d658534ab8dabb6a11040d6ef108f932
Trust Issues <skunk.trustissues@gmail.com>
2014-02-14 16:59:25 -0800

    Added include everything.
    Added a file that includes everything.

---
+++ 1983Util.h
@@ -0,0 +1,12 @@
+#ifndef __1983_UTIL_H
+#ifndef __1983_UTIL_H
+
+#include "AnalogRangeButton.h"
+#include "DualLiveSpeed.h"
+#include "Math.h"
+#include "SolenoidPair.h"
+#include "StallableMotor.h"
+#include "Time.h"
+#include "ValueChangeTrigger.h"
+
+#endif
